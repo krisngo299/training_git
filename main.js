4068015d1f743c6c597a566aed39487702f25a96
Kris Ngo <ngoxthanhxhung@gmail.com>
2021-03-14 21:43:05 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+﻿var vueInstance = new Vue({
+    el: '#app',
+    data: {
+        title:'Dien thoai Samsung'
+    },
+    methods: {
+        say: function (text) {
+            return 'Hello' + text;
+        }
+    }
+});
+//he thong phan ung Reactivity
+console.log(vueInstance);
+
+
+setTimeout(() => {
+    vueInstance.title = 'Iphone';
+}, 3000);
